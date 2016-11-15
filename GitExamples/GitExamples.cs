2d9e1520ed490e3979392182d063e378c8744bb4
Zdeněk Novák <zdenek.novak@stonehengegames.com>
2016-11-15 11:06:47 +0100

    Seznam lidi je serazen abecedne dle prijmeni

--- GitExamples/GitExamples.cs
+++ GitExamples/GitExamples.cs
@@ -6,12 +6,12 @@ namespace GitExamples
     {
         public static int Main(string[] args)
         {
-            Console.WriteLine("Chromik Daniel");
             Console.WriteLine("Biza Martin");
-            Console.WriteLine("Novak Zdenek");
             Console.WriteLine("Bulicka Michal");
             Console.WriteLine("Cacek Filip");
             Console.WriteLine("Hercka Lubos");
+            Console.WriteLine("Chromik Daniel");
+            Console.WriteLine("Novak Zdenek");
             return 0;
         }
     }
