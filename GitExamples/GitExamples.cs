562df92043bd154f1cb3900fa4a63a387e57b2bc
Zdeněk Novák <zdenek.novak@stonehengegames.com>
2016-11-15 10:54:10 +0100

    Nahrazeno odsazeni pomoci tabulatoru za mezery

--- GitExamples/GitExamples.cs
+++ GitExamples/GitExamples.cs
@@ -2,11 +2,11 @@
 
 namespace GitExamples
 {
-	public class GitExamples
-	{
-		public static int Main(string[] args)
-		{
-			return 0;
-		}
-	}
+    public class GitExamples
+    {
+        public static int Main(string[] args)
+        {
+            return 0;
+        }
+    }
 }
