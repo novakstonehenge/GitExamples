a65a386afaddd8cc6c97e69b025e36a5395d7af9
Zdeněk Novák <zdenek.novak@stonehengegames.com>
2016-11-15 12:31:46 +0100

    Program vypisuje seznam lidi pracujicich na CasinoLive

--- GitExamples/GitExamples.cs
+++ GitExamples/GitExamples.cs
@@ -6,6 +6,12 @@ namespace GitExamples
     {
         public static int Main(string[] args)
         {
+            Console.WriteLine("Chromik Daniel");
+            Console.WriteLine("Cacek Filip");
+            Console.WriteLine("Novak Zdenek");
+            Console.WriteLine("Hercka Lubos");
+            Console.WriteLine("Biza Martin");
+            Console.WriteLine("Bulicka Michal");
             return 0;
         }
     }
